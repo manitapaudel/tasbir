69807a31deb43b29f3ec915439f56ebdafc18609
Manita Paudel <paudel04manita@gmail.com>
2024-11-15 19:45:59 +0545

    Add skeleton loaders, customize its theme

---
+++ src/components/ImageCardSkeletonGrid.tsx
@@ -0,0 +1,17 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const ImageCardSkeleton = () => {
+  return (
+    <>
+      {Array.from({ length: 12 }).map((_, index) => (
+        <div className="flex flex-col gap-3" key={index}>
+          <Skeleton height={200} className=" h-auto" />
+          <Skeleton height={30} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ImageCardSkeleton;
