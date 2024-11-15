d627ff89d57676a82809536c767fa432ee786878
Manita Paudel <paudel04manita@gmail.com>
2024-11-15 12:48:35 +0545

    Fetch images data

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://picsum.photos/v2/list?page=${page}&limit=10`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch images.");
+        }
+
+        const data = await response.json();
+        setImages(data);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, [page]);
+
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
   return <div className="bg-gallery-bg h-auto py-10 px-20">Home Alone!</div>;
 };
 
