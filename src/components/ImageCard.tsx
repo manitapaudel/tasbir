dfbf2f3e8990d4a309a559d9804551ba434e898b
Manita Paudel <paudel04manita@gmail.com>
2024-11-17 09:07:35 +0545

    Add hover styles to image card

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+import ExpandIcon from "./icons/ExpandIcon";
+
 type ImageCardProps = {
   url: string;
   author: string;
@@ -5,13 +7,14 @@ type ImageCardProps = {
 
 const ImageCard = ({ url, author }: ImageCardProps) => {
   return (
-    <div className="max-h-96 rounded-md hover:cursor-pointer hover:animate-wiggle bg-white p-4">
+    <div className="relative group max-h-96 rounded-md hover:cursor-pointer hover:animate-wiggle bg-white p-4">
       <img
         src={url}
         alt=""
         width="400"
-        className="h-[300px] w-full rounded-t-md"
+        className="h-[300px] w-full rounded-t-md group-hover:opacity-50"
       />
+      <ExpandIcon extraClass="absolute hidden group-hover:block top-1/2 left-1/2" />
       <p className="font-indieFlower lg:text-[1.5vw] text-center py-2">
         {author}
       </p>
