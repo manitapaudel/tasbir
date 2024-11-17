6cd81e9be2858a38db92dd0cfdec08f7568cb419
Manita Paudel <paudel04manita@gmail.com>
2024-11-17 19:03:54 +0545

    Header responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
     <header className="text-center bg-gray-beige-100 py-8">
-      <h1 className="uppercase font-semibold font-inconsolata text-6xl tracking-wider">
+      <h1 className="uppercase font-semibold font-inconsolata text-4xl 500:text-5xl sm:text-6xl tracking-wider">
         Tasbir Studios.
       </h1>
-      <div className="text-2xl font-medium font-montserrat mt-3">
+      <div className="text-xl sm:text-2xl font-medium font-montserrat mt-3">
         Our focus is locked on{" "}
-        <div className="flex justify-center items-center text-6xl font-bold space-x-2 mt-2">
+        <div className="flex justify-center items-center text-4xl sm:text-6xl font-bold space-x-2 mt-2">
           <span className="animate-textFocus">Y</span>
           <span className="animate-textFocus">O</span>
           <span className="animate-textFocus">U</span>
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Header from "../components/icons/Header";
 
 const Root = () => {
   return (
