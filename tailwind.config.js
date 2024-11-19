f809e25e82e85b7a4a286a7da8dd0c7bc312e2b3
Manita Paudel <paudel04manita@gmail.com>
2024-11-19 13:26:03 +0545

    Remove unused colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        "gray-beige-100": "#F5F3F0", // Light Gray-Beige
-        "gray-beige-200": "#E0D9D0", // Soft Beige
-        "gray-beige-300": "#D1C8B6", // Warm Beige
-        "gray-beige-400": "#B8A895", // Muted Beige
-        "gray-beige-500": "#9E8C7B", // Medium Gray-Beige
-        "gray-beige-600": "#8A7766", // Dusty Beige
-        "gray-beige-700": "#7D6B59", // Rich Gray-Beige
-        "gray-beige-800": "#6A5A47", // Deep Beige
-        "gray-beige-900": "#5A4A3C", // Dark Gray-Beige
+        "gray-beige-100": "#F5F3F0",
+        "gray-beige-200": "#E0D9D0",
       },
       fontFamily: {
         montserrat: '"Montserrat", "serif"',
